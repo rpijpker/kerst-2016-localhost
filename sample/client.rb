433329df006610f7ea3a260a06736ddd5b12f6ff
rpijpker <r.pijpker@nerds.company>
2016-12-07 12:32:59 +0100

    removed port

--- sample/client.rb
+++ sample/client.rb
@@ -2,15 +2,18 @@
 $:.unshift File.expand_path '../lib', File.dirname(__FILE__)
 require 'rubygems'
 require 'websocket-client-simple'
+require 'hue'
+require 'sonos'
 
 puts "websocket-client-simple v#{WebSocket::Client::Simple::VERSION}"
 
-url = ARGV.shift || 'ws://localhost:8080'
-
-ws = WebSocket::Client::Simple.connect url
+ws = WebSocket::Client::Simple.connect 'ws://kerst-2016-server.herokuapp.com'
 
 ws.on :message do |msg|
   puts ">> #{msg.data}"
+  if msg.to_s == 'christmas'
+    start_christmas
+  end
 end
 
 ws.on :open do
@@ -24,6 +27,65 @@ end
 
 ws.on :error do |e|
   puts "-- error (#{e.inspect})"
+end
+
+def start_christmas
+  initialize_variables
+  # zodat alex niet boos wordt :(
+  @light.brightness = 1
+
+  @time = Time.now
+  play
+  @speaker.play
+  cycle
+end
+
+def play
+  @speaker.play 'x-sonos-spotify:spotify%3atrack%3a66mB55sZuDHlXt3vAcVkXf?sid=9&amp;flags=0'
+  @speaker.volume = 5
+  @speaker.play
+end
+
+def stop
+  @speaker.stop
+end
+
+def cycle
+  initialize_variables
+  color = [@light.x, @light.y]
+  while @speaker.is_playing?
+    sleep 2
+    turn_red
+    sleep 2
+    turn_green
+
+    @speaker.stop if Time.now > @time + 11
+  end
+  @light.set_state(xy: [color[0], color[1]])
+end
+
+def turn_green
+  @light.set_state(xy: [0.05, 0.85])
+end
+
+def turn_red
+  @light.set_state(xy: [0.7, 0.3])
+end
+
+def initialize_variables
+  @client = Hue::Client.new
+  @light = @client.lights.fetch(1)
+  while @light.name != 'Hue color lamp 2'
+    @client = Hue::Client.new
+    @light = @client.lights.fetch(1)
+  end
+
+  @system = Sonos::System.new
+  @speaker = @system.speakers.fetch(1)
+  while @speaker.name != 'Kantine'
+    @system = Sonos::System.new
+    @speaker = @system.speakers.fetch(1)
+  end
 end
 
 loop do

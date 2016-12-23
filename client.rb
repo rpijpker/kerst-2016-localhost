976e14dae321eb2fe7bd42fe84bd79e844de3b9c
Matthijs & Rimme <m.brink@nerds.company>
2016-12-23 12:26:42 +0100

    Fixed small error

--- client.rb
+++ client.rb
@@ -10,20 +10,6 @@ ws = WebSocket::Client::Simple.connect 'wss://kerst-2016-server.herokuapp.com:44
 
 ws.on :message do |msg|
   puts ">> #{msg.data} " + Time.now.to_s
-  if msg.to_s == 'Driving home for Christmas'
-    start_christmas('x-sonos-spotify:spotify%3atrack%3a66mB55sZuDHlXt3vAcVkXf?sid=9&amp;flags=0')
-  elsif msg.to_s == 'All I want for Christmas is you'
-    start_christmas('x-sonos-spotify:spotify%3atrack%3a5KdPyhcsQ5PvkvPBq6lLti?sid=9&amp;flags=0')
-  elsif msg.to_s == 'Last Christmas'
-    start_christmas('x-sonos-spotify:spotify%3atrack%3a0QPYn15U8IQHKcH2LDfrek?sid=9&amp;flags=0')
-  elsif msg.to_s == "It's beginning to look a lot like Christmas"
-    start_christmas('x-sonos-spotify:spotify%3atrack%3a5a1iz510sv2W9Dt1MvFd5R?sid=9&amp;flags=0')
-  elsif msg.to_s == 'Jingle bell rock'
-    start_christmas('x-sonos-spotify:spotify%3atrack%3a2wCPMWR3y4xclijuCcLJv7?sid=9&amp;flags=0')
-  elsif msg.to_s == 'Rudolph the red-nosed reindeer'
-    start_christmas('x-sonos-spotify:spotify%3atrack%3a4o8Zh55Xh9sBPJCM1g0Lf7?sid=9&amp;flags=0')
-  end
-
   stop if msg.to_s == 'Stop'
 
   start_christmas('x-sonos-spotify:spotify%3atrack%3a66mB55sZuDHlXt3vAcVkXf?sid=9&flags=0') if msg.to_s == 'christmas'
@@ -40,16 +26,15 @@ end
 ws.on :error do |e|
   puts "-- error (#{e.inspect})" + "\n"
   puts Time.now.to_s
-  ws.connect('ws://kerst-2016-server.herokuapp.com')
+  ws.connect('wss://kerst-2016-server.herokuapp.com')
 end
 
 ws.on :close do
-  ws.connect('ws://kerst-2016-server.herokuapp.com')
+  ws.connect('wss://kerst-2016-server.herokuapp.com')
 end
 
 def start_christmas(song)
   initialize_variables
-  @speaker.volume = 5
   @time = Time.now
   play(song)
   @speaker.play
@@ -57,13 +42,13 @@ def start_christmas(song)
 end
 
 def play(song)
-  song.to_s.slice! '?sid=9&amp;flags=0'
-  song.to_s.slice! '?sid=9&flags=0'
-  if @speaker.now_playing[:uri] == song + '?sid=9&flags=0'
-    @speaker.play
-  else
-    @speaker.play song + '?sid=9&amp;flags=0'
-  end
+  #song.to_s.slice! '?sid=9&amp;flags=0'
+  #song.to_s.slice! '?sid=9&flags=0'
+  #if @speaker.now_playing[:uri] == song + '?sid=9&flags=0'
+  #  @speaker.play
+  #else
+    @speaker.play song #+ '?sid=9&amp;flags=0'
+  #end
 end
 
 def stop
